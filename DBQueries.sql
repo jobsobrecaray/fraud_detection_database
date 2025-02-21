fd179821c24f37b286a2c5d945fe120c5f9cd990
jobsobrecaray <144820731+jobsobrecaray@users.noreply.github.com>
2025-02-21 19:17:45 -0400

    Update DBQueries.sql

--- DBQueries.sql
+++ DBQueries.sql
@@ -17,3 +17,21 @@ CREATE TABLE EntityType (
     EntityID INT PRIMARY KEY,
     DateIssued DATE 
 );
+
+CREATE TABLE Passwords (
+    PasswordID INT PRIMARY KEY,
+    FOREIGN KEY (AccountID) REFERENCES  INT
+    DateIssued DATE 
+);
+
+CREATE TABLE User (
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(255) NOT NULL,
+    Address VARCHAR(255),
+    Email VARCHAR(255) UNIQUE NOT NULL,
+    SIN_Number VARCHAR(20) UNIQUE NOT NULL,
+    Phone_Number VARCHAR(20),
+    DOB DATE NOT NULL,
+    Client_In_Other_Bank BOOLEAN DEFAULT FALSE,
+    Status VARCHAR(50)
+);
