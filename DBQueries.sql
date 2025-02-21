ccfece8adcb0e372084d411d9e31b083a3b6a811
jobsobrecaray <144820731+jobsobrecaray@users.noreply.github.com>
2025-02-21 19:05:04 -0400

    Update DBQueries.sql

--- DBQueries.sql
+++ DBQueries.sql
@@ -13,12 +13,7 @@ CREATE TABLE Account (
     FOREIGN KEY (EntityID) REFERENCES EntityType(EntityID) NOT NULL 
 );
 
-CREATE TABLE Account (
-    AccountID INT PRIMARY KEY,
-    AccountCreationDate DATE,
-    Balance INT,
-    Currency VARCHAR(50) NOT NULL,
-    accountStatus VARCHAR(50) NOT NULL,
-    UserType VARCHAR(100) NOT NULL UNIQUE,
-    FOREIGN KEY (EntityID) REFERENCES EntityType(EntityID) NOT NULL 
+CREATE TABLE EntityType (
+    EntityID INT PRIMARY KEY,
+    DateIssued DATE 
 );
